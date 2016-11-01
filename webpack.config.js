bc26ce8426f65d7eee54e25c513d57578889255d
miao yu <miaoyu2009@qq.com>
2016-11-01 14:25:35 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    entry: './index.js',
+    entry: './src/index.js',
     output: {
-        path: __dirname,
-        filename: 'bundle.js'
+        path: './build',
+        filename: 'jsBundle.js'
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
